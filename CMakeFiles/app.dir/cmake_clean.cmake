4b0794b45c1661d815e32596686994bff9fe1c71
sszp <aaaaaa1964027610@163.com>
2025-04-26 03:15:28 +0800

    temporal test

---
+++ CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/test.cpp.obj"
+  "CMakeFiles/app.dir/test.cpp.obj.d"
+  "F:/MyProjects/little_reflect/bin/app.exe"
+  "F:/MyProjects/little_reflect/bin/app.exe.manifest"
+  "F:/MyProjects/little_reflect/bin/app.pdb"
+  "F:/MyProjects/little_reflect/bin/libapp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
